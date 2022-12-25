40e019ae885dffd6a740d41ac0d5dd6de9d31ca2
Dagem D Worku <dagemdworku@gmail.com>
2022-12-25 06:29:33 +0300

    reader play list mobile

---
+++ src/constants/reader-tabs-constants.ts
@@ -0,0 +1,6 @@
+import { BookOpenIcon, ListBulletIcon } from "@heroicons/react/24/solid";
+
+export const readerTabs = [
+  { id: "reader_book_tab", name: "Book", icon: BookOpenIcon },
+  { id: "reader_chapters_tab", name: "Chapters", icon: ListBulletIcon },
+];
