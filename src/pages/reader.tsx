92646738ff737eb28000fa6bb9cfe360e1168cb7
Dagem D Worku <dagemdworku@gmail.com>
2022-12-28 17:09:13 +0300

    reader interface update

--- src/pages/reader.tsx
+++ src/pages/reader.tsx
@@ -1,24 +1,12 @@
-import { Tab } from "@headlessui/react";
 import { FunctionComponent } from "react";
 import { useSnapshot } from "valtio";
-import PlayList from "../components/lists/play-list";
 import AudioPlayer from "../components/players/audio-player";
-import BookPlayer from "../components/players/book-player";
-import { readerTabs } from "../constants/reader-tabs-constants";
 import PlayListService from "../services/feature/play-list-service";
 import { ServiceLocator } from "../services/service-locator";
-import { classNames } from "../utils/class-helper";
 
 interface ReaderPageProps {}
 
 const ReaderPage: FunctionComponent<ReaderPageProps> = () => {
-  const scrollToWord = document.getElementById("scroll-to-word");
-  if (scrollToWord) {
-    scrollToWord.scrollIntoView({
-      behavior: "smooth",
-      block: "center",
-    });
-  }
   const playListService: PlayListService = ServiceLocator.resolve(
     PlayListService.name
   );
@@ -28,76 +16,28 @@ const ReaderPage: FunctionComponent<ReaderPageProps> = () => {
   const playList = playListServiceSnapshot.playList;
   const currentChapter = playListServiceSnapshot.currentChapter;
   return (
-    <div className="flex flex-col h-full px-6 py-5 overflow-hidden bg-bg-l dark:bg-bg-d">
-      <div className="flex-none">
-        <div className="flex flex-col sm:items-end sm:flex-row">
-          <div className="flex items-end flex-1">
-            {/* Book artwork */}
-            <div className="w-24 h-24 overflow-hidden rounded-md sm:w-32 sm:h-32 bg-bg-l-s-i dark:bg-bg-d-s-i">
-              <img
-                className="w-full h-full"
-                src={playList?.artwork}
-                alt="Cover"
-              />
-            </div>
-
-            {/* Book: Title, Author, and published year */}
-            <div className="flex-1 ml-4">
-              <h3 className="text-2xl font-semibold text-fg-l-s dark:text-fg-d-s">
-                {playList?.name}
-              </h3>
-              <h2 className="text-base font-medium text-fg-l-s-i dark:text-fg-d-s-i">
-                {playList?.author}
-              </h2>
-              <h2 className="text-sm font-medium text-fg-l-s-i dark:text-fg-d-s-i">
-                {playList?.published}
-              </h2>
-            </div>
-          </div>
-          <div className="mt-6 sm:flex-1 sm:my-0">
-            <AudioPlayer isMobile={true} />
-          </div>
-        </div>
-        <div className="my-2">
-          <h1 className="text-xl font-semibold text-center text-fg-l dark:text-fg-d sm:text-start">
-            {currentChapter}
-          </h1>
+    <div className="flex flex-col h-full overflow-hidden bg-bg-l dark:bg-bg-d">
+      <div className="relative flex-1 overflow-hidden bg-bg-l dark:bg-bg-d">
+        <div
+          style={{
+            backgroundImage: `url(${playList?.artwork})`,
+          }}
+          className="absolute inset-0 scale-110 bg-center bg-cover blur-md opacity-60"
+        ></div>
+        <div className="absolute inset-0 grid items-center justify-center">
+          <img
+            className="rounded-lg shadow-md w-52"
+            src={playList?.artwork}
+            alt="Cover Image"
+          />
         </div>
       </div>
-
-      <div className="flex-1 hidden mt-3 space-x-6 overflow-hidden sm:flex">
-        <BookPlayer />
-        <PlayList />
-      </div>
-
-      <div className="flex flex-col flex-1 overflow-hidden sm:hidden">
-        <Tab.Group>
-          <Tab.List className="flex p-1 space-x-2 border rounded-lg bg-bg-l-s dark:bg-bg-d-s border-b-l dark:border-b-d">
-            {readerTabs.map((tab) => (
-              <Tab
-                key={tab.id}
-                className={({ selected }) =>
-                  classNames(
-                    "w-full rounded-md px-2 py-1.5 text-sm font-medium leading-5 ring-bg-l dark:ring-bg-d ring-opacity-60 ring-offset-2 ring-offset-p focus:outline-none focus:ring-2",
-                    selected
-                      ? "bg-bg-l dark:bg-bg-d shadow text-p"
-                      : "text-fg-l-s-i dark:text-fg-d-s-i hover:text-fg-l dark:hover:text-fg-d"
-                  )
-                }
-              >
-                {tab.name}
-              </Tab>
-            ))}
-          </Tab.List>
-          <Tab.Panels className="mt-2 overflow-hidden">
-            <Tab.Panel className="h-full overflow-hidden">
-              <BookPlayer />
-            </Tab.Panel>
-            <Tab.Panel className="h-full overflow-hidden">
-              <PlayList />
-            </Tab.Panel>
-          </Tab.Panels>
-        </Tab.Group>
+      <div className="w-full px-8 py-6 ">
+        <div className="flex flex-col w-full space-y-1">
+          <p className="text-center heading-5">{playList?.name}</p>
+          <p className="text-center body-intro">{currentChapter}</p>
+        </div>
+        <AudioPlayer />
       </div>
     </div>
   );
